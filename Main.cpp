5f8f4b0f0e30eaa42fa08e90c71f8939d3c76e4e
Rodrigo Velasquez <rodrix183@hotmail.com>
2018-11-02 14:24:12 -0600

    Me compilo papus :v liberar la matriz

--- Main.cpp
+++ Main.cpp
@@ -6,19 +6,81 @@ using std::endl;
 
 int*** crearCubo();
 
-void liberarMatrix();
+void provisionarMatrix(int);
+
+void liberarMatrix(int***);
 
 int main(){
     
     bool vive = true;
     int turno = 0;
+    int op1 = 0;
+    int op2 = 0;
+    //vidas de los jugadores
+    int vidas1 = 15;
+    int vidas2 = 15;
+
+    //submarinos
+    //int 1sub1 = 1;
+    //int 1sub2 = 1;
+    
     do{
         if(turno % 2 == 0){
             //aqui jugador 1
-            int sub1 = 0;
+            cout<<"1. Ataque Normal"<<endl
+            <<"2. Ataque Wave XY"<<endl
+            <<"3. Ataque Wave XZ"<<endl
+            <<"4. Ataque Wave YZ"<<endl
+            <<"Ingrese una ataque";
+            cin>>op1;
+            switch(op1){
+                case 1:{
+              
+                 }break;
+
+                case 2:{
+
+                }break;
+
+                case 3:{
+
+                }break;
+
+                case 4:{
+
+                }break;
+                
+                default:
+                    cout<<"Opcion no valida pierde turno!"<<endl;
+            }
         }else if(turno % 2 != 0){
-            //aqui jugador 2
-            int sub2 = 0;
+            //aqui jugador 
+            
+            cout<<"1. Ataque Normal"<<endl
+            <<"2. Ataque Wave XY"<<endl
+            <<"3. Ataque Wave XZ"<<endl
+            <<"4. Ataque Wave YZ"<<endl;
+            cin>>op2;
+            switch(op2){
+                case 1:{
+
+                }break;
+
+                case 2:{
+
+                }break;
+
+                case 3:{
+
+                }break;
+                
+                case 4:{
+
+                }break;
+
+                default:
+                cout<<"Opcion no valida pierde turno!"<<endl;
+            }
         }
 
         turno++;
@@ -44,6 +106,23 @@ int*** crearCubo(){
     return cubo;
 }
 
+void liberarMatrix(int*** matriz){
 
+    for(int i = 0; i < 12; i++){
+        for(int j = 0; j < 12; j++){
+            delete[] matriz[i][j];
+            matriz[i][j] = NULL;
+        }
+    }
+
+    for(int i = 0; i < 12; i++){
+        delete[] matriz[i];
+        matriz[i] = NULL;
+    }
+
+        delete[] matriz;
+        matriz = NULL;
+
+}
 
 
